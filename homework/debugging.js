6ad499e5d7be3677fd2eebf1f8fd264376d3460d
celeste-nunez <nun22002@byui.edu>
2024-09-24 10:06:41 -0600

    changes

--- homework/debugging.js
+++ homework/debugging.js
@@ -1,8 +1,31 @@
-const PI = 3.14;
-let radius = 3;
+const pi = 3.14;
 let area = 0;
-area = radius * radius * PI;
-console.log("Area1:", area);
-radius = 4;
-area = radius * radius * PI;
-colsole.log("Area2:", area);
+
+function circleArea(radius) {
+  const area = radius * radius * pi;
+}
+
+area = circleArea(3);
+console.log(area);
+
+area = circleArea(4);
+console.log(area)
+
+
+
+
+
+
+
+
+// const PI = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// console.log("Area1:", area);
+// radius = 4;
+// area = radius * radius * PI;
+// colsole.log("Area2:", area);
+
+
+
