a041e71dbd8fb10a433d87534c2ecd009238af76
celeste-nunez <nun22002@byui.edu>
2024-10-02 09:25:26 -0600

    delete bad one

--- wdd131-bad one/wk1/sandbox/running.js
+++
@@ -1,9 +0,0 @@
-const apples = 5;
-const oranges = 3;
-let total = apples + oranges;
-console.log("total:", total);
-
-const newImage = document.createElement("img");
-newImage.setAttribute("src", "https://picsum.photos/200");
-newImage.setAttribute("alt", "Random image from picsum.");
-document.body.appendChild(newImage);
---
+++ wk3/homework/arrays.js
@@ -0,0 +1,9 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+const listTemplate(step) {
+    const listElement = document.getElementById("myList");
+    listElement.innerHTML += `<li>${item}</li>`;
+  return //the html string made from step
+}
+const stepsHtml = // use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = // set the innerHTML
