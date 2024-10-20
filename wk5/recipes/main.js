0ca3549e503962a58b56ed9db8c9ebfb77a5982c
celeste-nunez <nun22002@byui.edu>
2024-10-19 21:33:33 -0600

    searchElement

--- wk5/recipes/main.js
+++ wk5/recipes/main.js
@@ -1,7 +1,7 @@
 import { recipes } from './recipes.js';
 
 function random(listLength) {
-    randomNum = Math.floor(Math.random()* listLength);
+    const randomNum = Math.floor(Math.random()* listLength);
     return randomNum
 }
 
@@ -13,7 +13,7 @@ function getRandomListEntry(list) {
 
 function renderRecipes(recipe) {
     return `
-    <section class="dessertBlock">
+    <section class="recipesBlock">
         <div class="recipeImg"><img src="${recipe.recipeImg}" alt="recipeImg"></div>
         <div class="recipeContent">
             <div class="recipeName"><h2>${recipe.recipeName}</h2></div>      
@@ -22,7 +22,6 @@ function renderRecipes(recipe) {
                 role="img"
                 aria-label="Rating: ${recipe.recipeStars}"
             >
-                // following 2 lines curtosey of chatGPT
                 ${'⭐'.repeat(Math.floor(recipe.recipeStars))}
                 ${'☆'.repeat(5 - Math.floor(recipe.recipeStars))}
             </span>
@@ -35,8 +34,9 @@ function renderRecipes(recipe) {
 function init() {
     const recipe = getRandomListEntry(recipes);
     const recipeHTML = renderRecipes(recipe);
-    document.body.innerHTML += recipeHTML;
+    document.querySelector('.recipesBlock').innerHTML = recipeHTML;
 }
+init();
 
 function filter(query) {
 	const filtered = recipes.filter(recipe =>
@@ -44,23 +44,29 @@ function filter(query) {
         recipe.description.toLowerCase().includes(query.toLowerCase())
     );
 	const sorted = filtered.sort((a,b) => a.recipeName.localeCompare(b.recipeName));
-	return sorted
+	return sorted;
 }
 
 function searchHandler(e) {
 	e.preventDefault()
-	// get the search input
-  // convert the value in the input to lowercase
-  // use the filter function to filter our recipes
-  // render the filtered list
 
+	const query = document.querySelector('.searchEntry').value;
+    const filteredRecipes = filter(query);
+
+    document.querySelector('.recipesBlock').innerHTML = '';
+
+    filteredRecipes.forEach(recipe => {
+        const recipeHTML = renderRecipes(recipe);
+        document.querySelector('.recipesBlock').innerHTML += recipeHTML;
+    })
 }
 
-
-
-
-init();
-
-console.log(getRandomListEntry(recipes));
 const randomRecipe = getRandomListEntry(recipes);
-console.log(recipeTemplate(randomRecipe));
+console.log(renderRecipes(randomRecipe));
+document.querySelector('.submit').addEventListener('click', searchHandler);
+
+document.querySelector('.searchEntry').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        searchHandler(e);
+    }
+});
