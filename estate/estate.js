0ba299020c7f00d2ad78051ce9122f76988d069a
celeste-nunez <nun22002@byui.edu>
2024-10-24 09:39:23 -0600

    starting buy page

--- estate/estate.js
+++ estate/estate.js
@@ -1,10 +1,10 @@
 function toggleMenu() {
-    const menu = document.querySelector("nav");
+    const menu = document.querySelector(".nav-box");
     menu.classList.toggle("hide")
 }
 
 window.addEventListener("resize", function() {
-    const nav = document.querySelector('nav');
+    const nav = document.querySelector('.nav-box');
     
     if (window.innerWidth >= 950) {
       nav.classList.remove('hide');
@@ -14,3 +14,77 @@ window.addEventListener("resize", function() {
 });
 
 document.querySelector('.menuButton').addEventListener('click', toggleMenu);
+
+// ----------------------------------------END OF MENU DROP CODE
+
+document.querySelectorAll('.slider-container').forEach((sliderContainer, index) => {
+  let currentSlide = 0;
+  const slides = sliderContainer.querySelectorAll('.slide');
+
+  function showSlide(index) {
+    if (index >= slides.length) {
+      currentSlide = 0;
+    } 
+    else if (index < 0) {
+      currentSlide = slides.length -1;
+    } 
+    else {
+      currentSlide = index;
+    }
+    
+    slides.forEach(slide => {
+      slide.classList.remove('active');
+    });
+    
+    slides[currentSlide].classList.add('active');
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentSlide -1);
+  }
+
+  sliderContainer.querySelector('.next').addEventListener('click', nextSlide);
+  sliderContainer.querySelector('.prev').addEventListener('click', prevSlide);
+
+  showSlide(currentSlide);
+});
+
+// ----------------------------------------END OF SLIDE SHOWS
+
+import { properties } from './properties.js';
+
+console.log(properties);
+
+function renderProperties(properties) {
+  return `
+  <div class="propertyDisplay">
+  <div class="slider-container">
+      <div class="slider">
+        <div class="slide active">
+          <img src="${properties.propertyImg1}" alt="Image 1">
+        </div>
+        <div class="slide">
+          <img src="${properties.propertyImg2}" alt="Image 2">
+        </div>
+        <div class="slide">
+          <img src="${properties.propertyImg3}" alt="Image 3">
+        </div>
+      </div>
+    
+      <a class="prev" onclick="prevSlide()">&#10094;</a>
+      <a class="next" onclick="nextSlide()">&#10095;</a>
+    </div>
+    <p>Property Type: ${properties.propertyType}</p>
+    <p>Price: ${properties.propertyPrice}</p>
+    <p>Beds: ${properties.beds}</p>
+    <p>Baths: ${properties.baths}</p>
+    <p>Garage: ${properties.garage}</p>
+    <p>Address: ${properties.address}</p>
+    <p>Year Built: ${properties.yearBuilt}</p>
+</div>
+  `
+}
---
+++ estate/properties.js
@@ -0,0 +1,62 @@
+export const properties = [
+    {
+        propertyType: 'House',
+        propertyPrice: '410,000',
+        beds: '3',
+        baths: '2',
+        sqft: '1,500',
+        garage: '2 car',
+        address: [
+            'ulta strawberry dreams zero sugar',
+            'Rexburg, ID',
+            '83440'
+        ],
+        yearBuilt: '2003',
+        propertyImg1: 
+            '.images/exampleHome.jpg',
+        propertyImg2:
+            '.images/exampleHome1.jpg',
+        propertyImg3:
+            '.images/exampleHome2.jpg'
+    },
+    {
+        propertyType: 'House',
+        propertyPrice: '500,000',
+        beds: '6',
+        baths: '4',
+        sqft: '2,300',
+        garage: '3 car',
+        address: [
+            'chester the cheese rat',
+            'Rigby, ID',
+            '83442'
+        ],
+        yearBuilt: '2010',
+        propertyImg1: 
+            '.images/exampleHome.jpg',
+        propertyImg2:
+            '.images/exampleHome1.jpg',
+        propertyImg3:
+            '.images/exampleHome2.jpg'
+    },
+    {
+        propertyType: 'Condo',
+        propertyPrice: '220,000',
+        beds: '2',
+        baths: '1',
+        sqft: '800',
+        garage: 'none',
+        address: [
+            'smokey the banditttt',
+            'Idaho Falls, ID',
+            '83402'
+        ],
+        yearBuilt: '2012',
+        propertyImg1: 
+            '.images/exampleHome.jpg',
+        propertyImg2:
+            '.images/exampleHome1.jpg',
+        propertyImg3:
+            '.images/exampleHome2.jpg'
+    }
+]
